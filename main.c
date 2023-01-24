32ee720f1cfd4b5a8273ab3720d94bea80df2646
stevenokoth <stevenocholao@gmail.com>
2023-01-24 09:40:17 -0800

    corr

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 
 /**
  * main - Entry point
- *
  * Return: Always 0
  */
 int main(void)
@@ -39,7 +38,7 @@ int main(void)
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
 	_printf("Unknown:[%r]\n");
-	/*printf("Unknown:[%r]\n");*/
+	printf("Unknown:[%r]\n");
 	return (0);
 }
 
